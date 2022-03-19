d891be2804d7301e22599169c278d718ad05fb35
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-03-19 19:02:33 -0300

    feat: Added Tailwind with new project structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <h1 className='text-red-800'>
+        Hello World
+      </h1>
+    </>
+  )
 }
 
 export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <h1 className='text-red-800'>
+
+    </h1>
+      Hello World
+    </>
+  )
+}
+
+export default Home
